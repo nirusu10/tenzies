9e4541d2e146615788b46ead5b9a8ab6655b2027
Nils Matic <173457306+nirusu10@users.noreply.github.com>
2025-05-25 16:46:32 +0200

    set up main container

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import { createRoot } from 'react-dom/client'
+import App from './App'
+/*
+ * - Style everything to look like the slide
+ */
+const root = createRoot(document.getElementById('root'))
+root.render(<App />)
