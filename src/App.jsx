782e26703f5d497f51c78ed9042bf8494c6868e9
Nils Matic <173457306+nirusu10@users.noreply.github.com>
2025-05-25 17:11:47 +0200

    add Die component, more styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,26 @@
+import Die from './components/Die'
+
 export default function App() {
-  return <main>MAIN</main>
+  return (
+    <main>
+      <h1>Tenzies</h1>
+      <p>
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
+      <div className='dice-container'>
+        <Die value={1} />
+        <Die value={3} />
+        <Die value={2} />
+        <Die value={4} />
+        <Die value={4} />
+        <Die value={6} />
+        <Die value={5} />
+        <Die value={1} />
+        <Die value={4} />
+        <Die value={6} />
+      </div>
+      <button className='btn'>Roll</button>
+    </main>
+  )
 }
